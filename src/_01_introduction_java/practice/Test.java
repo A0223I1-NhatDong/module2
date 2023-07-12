46946cba88d0c807660f93512884848f9e99a9a8
A0223I1-NhatDong <nhatdong.209@gmail.com>
2023-07-12 19:31:01 +0700

    Nop bai tap

--- src/_01_introduction_java/practice/Test.java
+++ src/_01_introduction_java/practice/Test.java
@@ -1,7 +1,12 @@
 package _01_introduction_java.practice;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ener your name : ");
+        String name = sc.nextLine();
+        System.out.println("Xin chào :" + name);
     }
 }
